c7f74cc29cd5abcc40758426463977dffe1122bd
wxc <748916590@qq.com>
2019-03-09 21:18:44 +0800

    首次开发

---
+++ src/main/java/com/laiyuan/zslaiyuan/ZslaiyuanApplication.java
@@ -0,0 +1,13 @@
+package com.laiyuan.zslaiyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZslaiyuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZslaiyuanApplication.class, args);
+	}
+
+}
